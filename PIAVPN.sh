5b03ce97384b3abd278449950f62787e3fbb37b6
Mandraxed <mandraxpython@gmail.com>
2017-01-04 11:25:07 +0000

    Update PIAVPN.sh

--- PIAVPN.sh
+++ PIAVPN.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # (c) 2017 Paul Roche
-# Program to install Private Internet Access VPN on Fedora 25 64-bit and Linux Debian PowerPC 32-bit
+# Program to install Private Internet Access VPN on virtually any Linux OS
+# (from Fedora 25 64-bit to Linux Debian PowerPC 32-bit)
 # Fedora 25 and Linux for PowerPC can present challenges when installing PIA VPN capability
 # Note that an account with Private Internet Access is required to enable the VPN
 # Version 0.1 - Amber Alligator
